6fc966734005dcf26210534c5d2705f52e6a7083
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2024-04-29 15:11:48 +0600

    footer link address added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
-import { FaFacebook, FaInstagram, FaBehance } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -8,10 +9,19 @@ const Footer = () => {
           <span>copyright Ethical Den</span>
           <span>hello@ethicalden.com</span>
         </small>
-        <div className="flex justify-center items-center gap-2">
-          <FaInstagram size={24} />
-          <FaFacebook size={24} />
-          <FaBehance size={28} />
+        <div className="flex justify-center items-center gap-2 md:gap-4">
+          <a href="https://github.com/Rakib-Hasan-Rakib" target="_blank">
+            <FaGithub size={24} />
+          </a>
+          <a href="https://www.facebook.com/rk.rakib.180625" target="_blank">
+            <FaFacebook size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rakibul-hasan-10a1b9274/"
+            target="_blank"
+          >
+            <FaLinkedin size={24} />
+          </a>
         </div>
       </div>
     </>
